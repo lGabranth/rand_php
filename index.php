842b5a3a42582c499ef82ca119cb289b611cc2d2
bep3 <martin.curtet@gmail.com>
2019-09-11 12:47:45 +0200

    Ajout d'une page de redirection

--- index.php
+++ index.php
@@ -9,65 +9,65 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
-    	body{
-    		background-color: gray;
-    	}
-    	.bloc{
-				background-color: white;
-				border: 2px solid #451209;
-				border-bottom-right-radius: 15px; 
-				border-bottom-left-radius: 15px; 
-				box-shadow: 1px 1px 1px black;
-    	}
+        body{
+            background-color: gray;
+        }
+        .bloc{
+                background-color: white;
+                border: 2px solid #451209;
+                border-bottom-right-radius: 15px; 
+                border-bottom-left-radius: 15px; 
+                box-shadow: 1px 1px 1px black;
+        }
     </style>
   </head>
   <body>
     <section class="container jumbotron text-center">
-    	<div class="row">
-    		<div class="col">
-    			<h1>Rand PHP</h1>
-    		</div>
-    	</div>
+        <div class="row">
+            <div class="col">
+                <h1>Rand PHP</h1>
+            </div>
+        </div>
     </section>
-		<!-- Créer un tableau, dans les 30 cases du tableau placer un nombre aléatoire entre 0 et 100.
-					Afficher le nombre de fois ou chaque valeur est présente dans le tableau. -->
-		<section class="container bloc">
-			<br>
-			<div class="row">
-				<div class="col">
-					<table border="double">
-						<tr>
-							<?php 
-								$valeurRandom = -1;
-								$tab = array();
+        <!-- Créer un tableau, dans les 30 cases du tableau placer un nombre aléatoire entre 0 et 100.
+                    Afficher le nombre de fois ou chaque valeur est présente dans le tableau. -->
+        <section class="container bloc">
+            <br>
+            <div class="row">
+                <div class="col">
+                    <table border="double">
+                        <tr>
+                            <?php 
+                                $valeurRandom = -1;
+                                $tab = array();
 
-								for ($i = 1; $i < 101; $i++) { 
-									$tabNombre[$i] = 0;
-								}
-								for ($i = 1 ; $i < 31 ; $i++) { 
-									$valeurRandom = rand(0,100);
-									$tab[$i] = $valeurRandom; 
-							?> 
-								<td><?php echo $tab[$i]; ?></td>
-							<?php } ?>
-						</tr>
-					</table>
-				</div>
-				<div class="col">
-					<?php 
-						for ($j = 1; $j < 31; $j++) { 
-							$tabNombre[$tab[$j]]++;
-						}
-						for ($i = 1; $i < 101; $i++) { 
-						?>
-							<p class="<?php echo ($tabNombre[$i] > 1) ? "alert alert-danger" : "" ?>">
-								<?php echo "La valeur ".($i)." est présente ".$tabNombre[$i]." fois."; ?>
-							</p>
-						<?php }
-					?>
-				</div>
-			</div>
-		</section>
+                                for ($i = 1; $i < 101; $i++) { 
+                                    $tabNombre[$i] = 0;
+                                }
+                                for ($i = 1 ; $i < 31 ; $i++) { 
+                                    $valeurRandom = rand(0,100);
+                                    $tab[$i] = $valeurRandom; 
+                            ?> 
+                                <td><?php echo $tab[$i]; ?></td>
+                            <?php } ?>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col">
+                    <?php 
+                        for ($j = 1; $j < 31; $j++) { 
+                            $tabNombre[$tab[$j]]++;
+                        }
+                        for ($i = 1; $i < 101; $i++) { 
+                        ?>
+                            <p class="<?php echo ($tabNombre[$i] > 1) ? "alert alert-danger" : "" ?>">
+                                <?php echo "La valeur ".($i)." est présente ".$tabNombre[$i]." fois."; ?>
+                            </p>
+                        <?php }
+                    ?>
+                </div>
+            </div>
+        </section>
 
 
 
